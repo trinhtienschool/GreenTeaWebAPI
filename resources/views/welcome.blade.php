b1bf6d0298f3a051827ddf40e6569391b8495af3
dan <abc>
2022-01-02 00:01:53 +0700

    hello world

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LARAVEL</title>
+    <link rel="stylesheet" href="{{asset('css/mdb.min.css')}}">
+</head>
+</head>
+<body>
+    <div class="text-center mt-5">
+        <button type="button" class="btn btn-primary " data-mdb-ripple-color="dark">
+            HELLO WORLD
+        </button>
+    </div>
+    <script src="{{asset('js/mdb.min.js')}}"></script>
+</body>
+</html>
